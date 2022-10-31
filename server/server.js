37a734322d5d11f068f94907cd9c0185a10a4712
notHim0 <shayantanweer27@gmail.com>
2022-10-31 15:52:17 +0530

    minor responsive adjustments

--- server/server.js
+++ server/server.js
@@ -24,4 +24,4 @@ app.get('', (req, res) => {
 	res.render('index', {});
 });
 
-app.listen(port, () => console.log('Server is up and running.....', serverDirectoryPath, viewsPath));
+app.listen(port, () => console.log('Server is up and running.....'));
